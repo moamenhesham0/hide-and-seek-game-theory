2d7ec2e60475a828853ff7904ecf342235dc6886
Ahmed Ali <154479514+AhmedAliElashmawy@users.noreply.github.com>
2025-05-19 21:11:37 +0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,8 +1,35 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "game_logic/ai_characters/hider.h"
+#include "gui/game_engine.h"
+#include "gui/main_menu.h"
 
-int main() {
-    simulate_games();
+
+int main(int argc, char* argv[])
+{
+    GameMenu *menu = menu_init("Start Menu", MENU_MIN_WIDTH, MENU_MIN_HEIGHT);
+
+    while(menu_run_status()){
+        menu_handle_events();
+        menu_render();
+        SDL_Delay(16);
+    }
+    bool play_game = menu->play_game;
+
+    menu_destroy();
+
+    if(play_game){
+    game_engine_init("Game Engine", MIN_WIDTH, MIN_HEIGHT);
+
+    while (game_engine_run_status())
+    {
+        game_engine_handle_events();
+        game_engine_render();
+        SDL_Delay(FRAME_RATE);
+    }
+
+    game_engine_destroy();
+}
+
+    IMG_Quit();
+    SDL_Quit();
+
     return 0;
 }
